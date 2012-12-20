a02a09fb2ad69ffd7b2961d45ba264f45d88d80b
Ken Rettberg <kenrettberg@gmail.com>
2012-12-20 15:32:58 -0800

    Figuring out a problem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MedicareBs::Application.config.secret_token = '6ec6472cfa959d36e69909fe0fb1f50297eb04dfd018d2bfd69ad96fcc4c6c3f2c80f625a1a569ce56190305c24af8df7486c72a102bc0afe1660807aacb873f'
