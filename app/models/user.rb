8f50f797fff1875beb8579d2b00c100eb2246b4f
Ken Rettberg <kenrettberg@gmail.com>
2012-12-24 12:51:15 -0800

    For Amay

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+lass User < ActiveRecord::Base
+  attr_accessible :name, :email
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+end
