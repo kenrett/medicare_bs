c216620239ade91736cf0afc30ea06b245563372
Ken Rettberg <kenrettberg@gmail.com>
2013-01-02 16:21:54 -0800

    Just a quick save

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 MedicareBs::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,61 +11,41 @@
 
 require 'spec_helper'
 
-describe User do
+describe "User pages" do
 
-  before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
   end
 
-  subject { @user }
+  describe "signup" do
 
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
+    before { visit signup_path }
 
-  it { should be_valid }
+    let(:submit) { "Create my account" }
 
-  describe "when name is not present" do
-    before { @user.name = " " }
-    it { should_not be_valid }
-  end
-
-   describe "when email is not present" do
-    before { @user.email = " " }
-    it { should_not be_valid }
-  end
-
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
-    it { should_not be_valid }
-  end
-
-  describe "when email format is invalid" do
-    it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
-      addresses.each do |invalid_address|
-        @user.email = invalid_address
-        @user.should_not be_valid
-      end      
-    end
-  end
-
-  describe "when email format is valid" do
-    it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-      addresses.each do |valid_address|
-        @user.email = valid_address
-        @user.should be_valid
-      end      
-    end
-  end
-
-  describe "when email address is already taken" do
-    before do
-      user_with_same_email = @user.dup
-      user_with_same_email.save
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
     end
 
-    it { should_not be_valid }
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 end
+
