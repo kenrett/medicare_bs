fd497291e054d7bba56a1d869e18bb83a5ca0e61
Ken Rettberg <kenrettberg@gmail.com>
2013-01-07 14:11:56 -0800

    Just before 9.2.2

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -36,10 +36,30 @@ describe "signin" do
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
-        describe "followed by signout" do
-          before { click_link "Sign out" }
-          it { should have_link('Sign in') } 
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') } 
+      end
+    end
+  end
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
         end
       end
+    end
   end
 end
