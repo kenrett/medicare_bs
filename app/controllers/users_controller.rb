b41ce5b6fa53512a91ee68156ae18f1d6c1ec87e
Ken Rettberg <kenrettberg@gmail.com>
2013-01-07 14:06:54 -0800

    Just before 9.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
- def create
+  def create
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
@@ -16,6 +16,21 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 end
